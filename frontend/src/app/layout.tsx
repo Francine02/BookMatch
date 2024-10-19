38fb4c6d1f43215763304a8b9755af1466d7c1b2
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-19 19:26:32 -0300

    style: added font style and page icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <link rel="icon" href="https://img.icons8.com/?size=100&id=K64BmSGjcwqt&format=png&color=000000" />
+        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="/_next/static/css/app/layout.css?v=1729284656896" />
       </head>
       <body>{children}</body>
     </html>
